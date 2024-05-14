9a7dfd75062c4811479819133520a18a54ab611a
aldozavala <160289969+aldozavala@users.noreply.github.com>
2024-05-14 00:11:34 +0000

    version 0

--- src/pantalla.cpp
+++ src/pantalla.cpp
@@ -16,21 +16,22 @@ int main(int argc, char const *argv[])
         pantalla.Clear();
         this_thread::sleep_for(0.1s);
 
-        pantalla.PixelAt(x,5).character = "   ⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠒⠛⠛⠓⢦⡀⠀⢀⣀⡀⠀⠀⠀⠀";
-        pantalla.PixelAt(x,6).character = "⠀⠀⠀⠀   ⢀⡾⠁⠀⠀⠀⣤⢦⢡⣷⣴⣛⠩⡙⠓⢦⡀⠀";
-        pantalla.PixelAt(x,7).character = "⠀⠀⠀⠀⠀⢠⡟⠀⠀⠀⢀⠤⠑⠈⠉⣀⣤⣤⣭⢷⣀⠀⡇⠀";
-        pantalla.PixelAt(x,8).character = "⠀⠀⠀⠀⣀⡟⠀⠀⡠⠊⠁⣀⠤⠂⠁⣬⣟⣉⠀⠀⢀⡼⠃⠀";
-        pantalla.PixelAt(x,9).character = "⠀⠀⠀⡞⠉⠃⠀⣼⣇⠈⠁⠀⢿⠀⠀⠉⠉⠉⠙⣦⠾⠁⠀⠀";
-        pantalla.PixelAt(x,10).character = "⠀⠀⠀⢧⡔⠈⡉⠻⣿⡷⠀⣤⣄⣀⡀⠀⠀⠀⢀⡾⠀⠀⠀⠀";
-        pantalla.PixelAt(x,11).character = "⠀⠀⠀⠸⡄⠀⠈⠀⠈⠁⠀⠙⢿⣿⣿⣷⣶⣶⠟⠁⠀⠀⠀⠀";
-        pantalla.PixelAt(x,12).character = "⠀⠀⠀⠀⠙⠲⣶⣶⣦⣀⠀⠀⢈⠉⡹⡻⣩⠏⠀⠀⠀⠀⠀⠀";
-        pantalla.PixelAt(x,13).character = "⠀⠀⣠⡤⠶⡋⠁⢀⡠⠜⢹⠒⠲⡓⢫⠻⣅⣀⡴⠶⠶⣤⡀⠀";
-        pantalla.PixelAt(x,14).character = "⠀⡴⠋⠀⠀⠀⢑⡏⠀⠀⡰⢠⠂⠈⠄⠀⠏⢻⠁⠀⠀⢸⢻⡆";
-        pantalla.PixelAt(x,15).character = "⢸⡃⠊⠰⠀⠀⠀⣏⠐⠈⠀⠈⠢⠤⠂⠀⠈⡟⠀⠀⢀⢢⡟⠀";
-        pantalla.PixelAt(x,16).character = "⠀⠳⣄⡀⠀⣲⠞⠉⠹⢦⡀⠀⠀⠀⠀⢀⣼⣇⡀⢀⢨⡟⠀⠀";
-        pantalla.PixelAt(x,17).character = "⠀⠀⠀⢹⠏⠇⠀⠀⠀⢸⠧⠤⠤⠶⠚⠋⠀⠈⠉⠙⠛⠁⠀⠀";
-        pantalla.PixelAt(x,18).character = "⠀⠀⠀⢼⢰⠀⠀⠀⢰⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
-        pantalla.PixelAt(x,19).character = "    ⠈⠛⠦⣤⡤⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⠀";
+        pantalla.PixelAt(x,1).character = "   ⠀⠀⠀⠀⠀⠀⠀⠀⣀⡴⠒⠛⠛⠓⢦⡀⠀⢀⣀⡀";
+        pantalla.PixelAt(x,2).character = "       ⢀⡾⠁⠀⠀⠀⣤⢦⢡⣷⣴⣛⠩⡙⠓⢦⡀";
+        pantalla.PixelAt(x,3).character = "⠀⠀⠀⠀⠀⢠⡟⠀⠀⠀⢀⠤⠑⠈⠉⣀⣤⣤⣭⢷⣀⠀⡇";
+        pantalla.PixelAt(x,4).character = "⠀⠀⠀⠀⣀⡟⠀⠀⡠⠊⠁⣀⠤⠂⠁⣬⣟⣉⠀⠀⢀⡼⠃";
+        pantalla.PixelAt(x,5).character = "⠀⠀⠀⡞⠉⠃⠀⣼⣇⠈⠁⠀⢿⠀⠀⠉⠉⠉⠙⣦⠾⠁";
+        pantalla.PixelAt(x,6).character = "⠀⠀⠀⢧⡔⠈⡉⠻⣿⡷⠀⣤⣄⣀⡀⠀⠀⠀⢀⡾";
+        pantalla.PixelAt(x,7).character = "⠀⠀⠀⠸⡄⠀⠈⠀⠈⠁⠀⠙⢿⣿⣿⣷⣶⣶⠟⠁";
+        pantalla.PixelAt(x,8).character = "⠀⠀⠀⠀⠙⠲⣶⣶⣦⣀⠀⠀⢈⠉⡹⡻⣩⠏";
+        pantalla.PixelAt(x,9).character = "⠀⠀⣠⡤⠶⡋⠁⢀⡠⠜⢹⠒⠲⡓⢫⠻⣅⣀⡴⠶⠶⣤⡀";
+        pantalla.PixelAt(x,10).character = "⠀⡴⠋⠀⠀⠀⢑⡏⠀⠀⡰⢠⠂⠈⠄⠀⠏⢻⠁⠀⠀⢸⢻⡆";
+        pantalla.PixelAt(x,11).character = "⢸⡃⠊⠰⠀⠀⠀⣏⠐⠈⠀⠈⠢⠤⠂⠀⠈⡟⠀⠀⢀⢢⡟⠀";
+        pantalla.PixelAt(x,12).character = "⠀⠳⣄⡀⠀⣲⠞⠉⠹⢦⡀⠀⠀⠀⠀⢀⣼⣇⡀⢀⢨⡟⠀";
+        pantalla.PixelAt(x,13).character = "⠀⠀⠀⢹⠏⠇⠀⠀⠀⢸⠧⠤⠤⠶⠚⠋⠀⠈⠉⠙⠛⠁";
+        pantalla.PixelAt(x,19).character = "⠀⠀⠀⢼⢰⠀⠀⠀⢰⡟";
+        pantalla.PixelAt(x,20).character = "    ⠈⠛⠦⣤⡤⠞⠁";
+        pantalla.PixelAt(x,21).character ="______________________________________________________________";
         pantalla.Print();
         cout<<pantalla.ResetPosition();
     }
